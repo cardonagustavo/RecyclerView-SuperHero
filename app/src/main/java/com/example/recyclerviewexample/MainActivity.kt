9d163836f459d65cb07b312145b58edea174642a
Gus <cardona50368@gmail.com>
2023-10-06 11:33:30 -0500

    Se crea el listOF y el FrameLayout

---
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.recyclerviewexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.recyclerviewexample.ui.theme.RecyclerViewExampleTheme
+
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/recyclerviewexample/SuperHeroList.kt
@@ -0,0 +1,108 @@
+package com.example.recyclerviewexample
+
+class SuperHeroList {
+    companion object{
+        val superHeroList = listOf<SuperHero>(
+            SuperHero(
+                "Android",
+                "Google",
+                "Android",
+                "https://developer.android.com/images/logos/android.svg"
+            ),
+
+            SuperHero(
+                "Spiderman",
+                "Marvel",
+                "Peter Parker",
+                "https://wallpapers.com/images/high/marvel-pictures-ddhaw72koaa7ewpx.webp"
+            ),
+
+            SuperHero(
+                "Capitan America",
+                "Marvel",
+                "Stive Rogers",
+                "https://wallpapers.com/images/high/marvel-pictures-iq4mv7at03xw8m7k.webp"
+            ),
+
+            SuperHero(
+                "Capitana Marver",
+                "Marvel",
+                "",
+                "https://wallpapers.com/images/high/marvel-pictures-h204orhtah6n58hk.webp"
+            ),
+
+            SuperHero(
+                "Iron Man",
+                "Marvel",
+                "Toni Stark",
+                "https://images.immediate.co.uk/production/volatile/sites/3/2018/05/IRON-2008-d7a2706.jpg?quality=90&webp=true&resize=800,534"
+            ),
+
+            SuperHero(
+                "Thor",
+                "Marvel",
+                "Thor",
+                "https://sinemahanedani.com/wp-content/uploads/2021/05/Thor-Kimdir-En-Guclu-Yenilmez-Olabilir-Sinema-Hanedani-3-850x480.jpg"
+            ),
+
+            SuperHero(
+                "Thanos",
+                "Marvel",
+                "Thanos",
+                "https://fotografias.antena3.com/clipping/cmsimages01/2018/05/21/4546D9ED-7807-43EB-8553-C8A4E3B8A2B1/97.jpg?crop=1212,682,x136,y0&width=1600&height=900&optimize=high&format=webply"
+            ),
+
+            SuperHero(
+                "Venom",
+                "Marvel",
+                "Venom",
+                "https://i.blogs.es/882897/venom/1366_2000.jpg"
+            ),
+
+            SuperHero(
+                "Viuda Negra",
+                "Marvel",
+                "Black widow",
+                "https://www.accioncine.es/wp-content/uploads/2014/02/78468ad5288d433009367eeff5a5f3b3.jpg"
+            ),
+
+            SuperHero(
+                "Hulk",
+                "Marvel",
+                "Hulk",
+                "https://www.tonica.la/__export/1678755721589/sites/debate/img/2023/03/13/mark-ruffalo-hulk.jpg_463833556.jpg"
+            ),
+
+            SuperHero(
+                "",
+                "",
+                "",
+                ""
+            ),
+
+            SuperHero(
+                "",
+                "",
+                "",
+                ""
+            ),
+
+            SuperHero(
+                "",
+                "",
+                "",
+                ""
+            ),
+
+            SuperHero(
+                "",
+                "",
+                "",
+                ""
+            ),
+
+
+
+            )
+    }
+}
